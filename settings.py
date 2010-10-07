e2bc6b70984c7d866a140abfd481bc75ffdaf482
frosch62 <curtisekstrom@gmail.com>
2010-10-07 12:10:00 +0200

    settings.py altered again

--- settings.py
+++ settings.py
@@ -102,7 +102,7 @@ MIDDLEWARE_CLASSES = (
     
 )
 
-ROOT_URLCONF = 'base-cms.urls'
+ROOT_URLCONF = 'basecms.urls'
 
 TEMPLATE_DIRS = ( os.path.join(PROJECT_ROOT, 'templates') 
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
